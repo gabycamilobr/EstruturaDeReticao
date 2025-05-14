15993ff222c20b3ec24939c6cf08f12647d1d496
Laboratório FIAP <labsfiap@fiap.com.br>
2025-05-13 22:45:23 -0300

    Finalizado

--- 09-Estrutura-de-Repeticao/src/Ex02.java
+++ 09-Estrutura-de-Repeticao/src/Ex02.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class Ex02 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int valor, cont = 0, resultado;
+        int resposta = 1;
+
+        while (resposta == 1){
+            System.out.println("Valor -- > ");
+            cont = 0;
+            valor = sc.nextInt();
+            while(cont <= 10){
+                resultado = valor * cont;
+                System.out.println(valor + " x " + cont + " = " + resultado);
+                cont = cont + 1;
+            }
+            System.out.println("Digite 1 para nova tabuada ou 0 para finalizar");
+            resposta = sc.nextInt();
+        }
 
     }
 }
